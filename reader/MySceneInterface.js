46bef71e2798635dd3c82b63002634663f0de5fa
RuiCS <rui.pedro.csoares@gmail.com>
2016-10-23 15:34:26 +0100

    light interface buggy

--- reader/MySceneInterface.js
+++ reader/MySceneInterface.js
@@ -7,8 +7,18 @@ MySceneInterface.prototype.constructor = MySceneInterface;
 
 MySceneInterface.prototype.init = function(application) {
     CGFinterface.prototype.init.call(this, application);
+	
+	this.gui = new dat.GUI();
+	
+	var group = this.gui.addFolder("Luzes");
+	group.open();
+	for (var i = 0; i < this.scene.lights.length; i++){
+		var name = 'light' + i;
+		group.add(this.scene, name);
+	}
     return true;
 };
+	
 
 MySceneInterface.prototype.processKeyDown = function(event) {
     switch (event.keyCode) 
