87ef6002c4a83ed77837e0891a9a19f534ce2aa8
nuno <nunojorge90@hotmail.com>
2016-12-04 18:04:06 +0000

    skyboxdone

--- trabalho3/reader/MyPrimSkybox.js
+++ trabalho3/reader/MyPrimSkybox.js
@@ -8,46 +8,48 @@ function MyPrimSkybox(scene,size) {
 MyPrimSkybox.prototype.constructor=MyPrimSkybox;
 
 MyPrimSkybox.prototype.display = function(){
-
+    //front
      this.scene.pushMatrix();
         this.scene.translate(0,0,this.size/2);
  	    this.scene.rotate(Math.PI,0,1,0);
         this.rect.setTexCoords(0.25,0.5,0.333,0.666);
         this.rect.display();
      this.scene.popMatrix();
-
+     //right
      this.scene.pushMatrix();
         this.scene.translate(-this.size/2,0,0);
  	    this.scene.rotate(Math.PI/2,0,1,0);
-        this.rect.setTexCoords(0.5,0.75,0.333,0.666);
+        this.rect.setTexCoords(0.5,0.75,0.3337,0.665);
         this.rect.display();
      this.scene.popMatrix();
-
+     //left
      this.scene.pushMatrix();
         this.scene.translate(this.size/2,0,0);
  	    this.scene.rotate(-Math.PI/2,0,1,0);
-        this.rect.setTexCoords(0,0.25,0.333,0.666);
+        this.rect.setTexCoords(0,0.25,0.3337,0.666);
         this.rect.display();
      this.scene.popMatrix();
-
+     //back
      this.scene.pushMatrix();
         this.scene.translate(0,0,-this.size/2);
  	    this.scene.rotate(0,0,1,0);
-        this.rect.setTexCoords(0.75,1,0.333,0.666);
+        this.rect.setTexCoords(0.75,1,0.3337,0.666);
         this.rect.display();
      this.scene.popMatrix();
-
+     //top
      this.scene.pushMatrix();
         this.scene.translate(0,this.size/2,0);
+        this.scene.rotate(Math.PI,0,1,0);
  	    this.scene.rotate(Math.PI/2,1,0,0);
-        this.rect.setTexCoords(0.25,0.5,0,0.333);
+        this.rect.setTexCoords(0.251,0.499,0.001,0.333);
         this.rect.display();
      this.scene.popMatrix();
-
+     //bottom
      this.scene.pushMatrix();
-        this.scene.translate(0,this.size/2,0);
- 	    this.scene.rotate(Math.PI/2,1,0,0);
-        this.rect.setTexCoords(0.5,0.75,0.666,1);
+        this.scene.translate(0,-this.size/2,0);
+        this.scene.rotate(Math.PI,0,1,0);
+ 	    this.scene.rotate(-Math.PI/2,1,0,0);
+        this.rect.setTexCoords(0.251,0.499,0.666,0.999);
         this.rect.display();
      this.scene.popMatrix();
  }
